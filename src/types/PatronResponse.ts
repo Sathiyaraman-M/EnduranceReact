7e89acc097ef23347f5c4ff41daf950c56b68631
SathiyaramanM <sathiyaraman2003@gmail.com>
2022-12-10 17:32:55 +0530

    Created basic type definitions

---
+++ src/types/PatronResponse.ts
@@ -0,0 +1,15 @@
+﻿export interface PatronResponse {
+    Id: string;
+    FirstName: string;
+    LastName: string;
+    Address: string;
+    DateOfBirth: Date;
+    Email: string;
+    Mobile: string;
+    RegisterId: string;
+    Issued: Date;
+    CurrentDues: number;
+    CheckoutsCount: number;
+    CheckoutsPending: number;
+    MultipleCheckoutLimit: number;
+} 
---
+++ src/types/Result.ts
@@ -0,0 +1,17 @@
+﻿export interface Result {
+    Message: string[];
+    Succeeded: boolean;    
+}
+
+export interface TypedResult<T> extends Result {
+    Data: T 
+}
+
+export interface PaginatedResult<T> extends TypedResult<T> {
+    CurrentPage: number;
+    HasNextPage: boolean;
+    HasPreviousPage: boolean;
+    PageSize: number;
+    TotalCount: number;
+    TotalPages: number
+}
