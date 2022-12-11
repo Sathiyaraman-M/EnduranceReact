a82245635e464f2f52ecd0a47be64d5522ff4325
SathiyaramanM <sathiyaraman2003@gmail.com>
2022-12-11 09:13:32 +0530

    Added headers to axios httpclient

--- src/services/base/http-common.ts
+++ src/services/base/http-common.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://localhost:7039'
+    baseURL: 'https://localhost:7039',
+    headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+    }
 })
